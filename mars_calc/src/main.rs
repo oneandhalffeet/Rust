9f2f7a68cbb233fbd4c10cd085cdf8b459738aa9
oneandhalfeet <uddeshteke007@gmail.com>
2023-08-17 21:45:47 +0530

    mars commit

--- mars_calc/src/main.rs
+++ mars_calc/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
     println!("Hello, world!");
+    let we = calc_weight_on_mars(100.0);
+    println!("My weight is {}", we);
+}
+
+fn calc_weight_on_mars(weight: f32) -> f32 {
+    weight*3.711/9.81
 }
