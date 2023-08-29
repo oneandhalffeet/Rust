8f2bc0b0846ced2d6fd6c606bf99bf87543eb2d8
oneandhalfeet <uddeshteke007@gmail.com>
2023-08-29 23:57:55 +0530

    Option enums for null values

--- server/src/main.rs
+++ server/src/main.rs
@@ -9,6 +9,11 @@ fn main() {
     // dbg!(&string_borrow);
     // dbg!(&string_literal);
 
+    // let get = Method::GET("abcd".to_string());
+    // let post = Method::POST;
+    // let delete = Method::DELETE(123);
+    // let put = Method::PUT;
+
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
@@ -31,3 +36,22 @@ impl Server {
         println!("Listening on {}", self.addr);
     }
 }
+
+struct Request{
+    path: String,
+    query_string: Option<String>,
+    method: Method
+}
+
+enum Method{
+    GET,
+    POST,
+    PUT,
+    HEAD,
+    DELETE,
+    PATCH,
+    OPTIONS,
+    CONNECT,
+    TRACE
+}
+
